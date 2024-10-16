aba1f6e1c2bd7077201531df9a9727c97d210b58
Júlia Du Bois <jujubadubois@gmail.com>
2024-10-16 20:57:39 -0300

    Beginning of dead man's trigger implementation

---
+++ serverClientGameThreads/gameClient.py
@@ -0,0 +1,92 @@
+# PEER TO PEER
+# CLIENT PEER
+
+import socket
+import time
+import threading
+
+import gameLibrary as g
+
+clientMatch:g.match = None
+QUIT:bool = False
+
+def deadMansTrigger():
+    totalWaitTime = 0
+    while totalWaitTime < 60:
+        totalWaitTime+=1
+        time.sleep(1)
+    return True
+
+def handleCommand(command, connection):
+    global clientMatch
+    global QUIT
+    if "SHOWBOARD" in command:
+        g.showBoard(clientMatch)
+    if "INITMATCH" in command:
+        temp = command.split(" ")
+        scores = [temp[-3], temp[-2], temp[-1]]
+        clientMatch = g.match(temp[1], temp[2], temp[3], temp[4], scores)
+    if "ASKPLAY" in command:
+        deadMansThread = threading.Thread(target=deadMansTrigger)
+        deadMansThread.start()
+        col = g.playerInput(2, clientMatch)
+        deadMansThread.join()
+        row = g.addPiece(2, col, clientMatch)
+        connection.send(f"{col}".encode())
+    if "REMOTEPLAY" in command:
+        col = command.split(" ")[-1]
+        row = g.addPiece(1, int(col), clientMatch)
+    if "NOTICE" in command:
+        msg = command.split(':')[1]
+        print(msg)
+    if "REPLAY" in command:
+        valid = True;
+        while valid:
+            replay = input("Replay?(y/n): ").lower()
+            if replay == "n":
+                valid = False
+                connection.send('n'.encode())
+            elif replay == "y":
+                valid = False
+                connection.send('y'.encode())
+    if "QUIT" in command:
+        QUIT = True
+
+
+def clienteProgram():
+
+    host = input("Host: ")
+    port = int(input("Port: "))
+
+    clientSocket = socket.socket() # instantiate
+    clientSocket.connect((host, port)) # connect to server
+
+    global QUIT
+
+    nomeServidor = clientSocket.recv(1024).decode() # gets server name (p1)
+    print(f"Connected to [{nomeServidor}]")
+
+    nomeCliente = input('Player name: ').lower() # gets client name (p2)
+    clientSocket.send(nomeCliente.encode())
+
+    print(f"Waiting for [{nomeServidor}]'s character selection...")
+    charP1 = clientSocket.recv(1024).decode() # pega o char de p1
+    print(f"[{nomeServidor}] chose the character [{charP1}]")
+
+    charP2 = " " # pega o char de p2
+    while charP2 == " " or charP2.lower() == charP1.lower() or len(charP2) > 1:
+        if charP2.lower() == charP1.lower():
+            print("Choose a different character")
+        elif len(charP2) > 1:
+            print("Choose only one character")
+        charP2 = input(f"{nomeCliente} character: ")
+    clientSocket.send(charP2.encode())
+
+    while not QUIT:
+        command = clientSocket.recv(1024).decode() # receives instruction
+        handleCommand(command, clientSocket) # passes the instruction to the handler
+
+    clientSocket.close() # close the connection
+
+if __name__ == '__main__':
+    clientThread = threading.Thread(target=clientProgram)
---
+++ serverClientGameThreads/gameServer.py
@@ -0,0 +1,170 @@
+# PEER TO PEER
+# SERVER PEER
+
+import socket
+import time
+import threading
+import queue
+
+from markdown_it.rules_core.normalize import NULL_RE
+
+import gameLibrary as g
+
+totalWaitTime = 0
+
+def deadMansTrigger():
+    global totalWaitTime
+    while True:
+        totalWaitTime+=1
+        fila.put(totalWaitTime)
+        time.sleep(1)
+
+def serverProgram():
+    # get hostname
+    hostname = socket.gethostname()
+    hostIP = socket.gethostbyname(hostname)
+    host=''
+    port = 0
+    while port<1024:
+        port = int(input('Choose a port: '))
+
+
+    print(f"Host: [{hostname}] on IP address [{hostIP}]")
+    print(f"Port: {port}")
+
+    serverSocket = socket.socket() #get instance
+    serverSocket.bind((host, port))
+
+    # configure how many clients can listen simultaneously (1)
+    serverSocket.listen(1)
+
+    nomeServidor = input("Player name: ").lower() # pega o nome do servidor (p1)
+
+    print('Waiting for a connection...')
+
+    conn, address = serverSocket.accept() # accept new connection
+    conn.send(nomeServidor.encode())
+
+    nomeCliente = conn.recv(1024).decode() # pega o nome do cliente (p2)
+    print(f"[{nomeCliente}] connection from {str(address)}")
+
+    charP1 = " " # pega o char de p1
+    while charP1 == " " or len(charP1) > 1:
+        if len(charP1) > 1:
+            print("Choose only one character")
+        charP1 = input(f"[{nomeServidor}] character: ")
+    conn.send(charP1.encode())
+
+    print(f"Waiting for [{nomeCliente}]'s character selection...")
+    charP2 = conn.recv(1024).decode() # pega o char de P2
+    print(f"[{nomeCliente}] chose the character [{charP2}]")
+
+    QUIT = False; GAME = True
+    PLAYER1 = 1; PLAYER2 = 2
+    HEIGHT = 6; WIDTH = 7
+    NUMFORWIN = 4
+    empitySpaces = HEIGHT * WIDTH
+    scores=[0, 0, 0]
+
+    while not QUIT:
+        currentMatch = g.match(nomeServidor, nomeCliente, charP1, charP2, scores)
+        conn.send(f"INITMATCH {nomeServidor} {nomeCliente} {charP1} {charP2} {scores[0]} {scores[1]} {scores[2]}".encode())
+        time.sleep(0.5)
+        turn = g.gameStarter()
+        if turn == 1:
+            turnP1 = True
+            msg = f"{nomeServidor} starts"
+            conn.send(f"NOTICE:{msg}".encode())
+            print(msg)
+            time.sleep(0.5)
+        else:
+            turnP1 = False
+            msg = f"{nomeCliente} starts"
+            conn.send(f"NOTICE:{msg}".encode())
+            print(msg)
+        g.showBoard(currentMatch)
+        conn.send("SHOWBOARD".encode())
+        time.sleep(0.5)
+        while GAME:
+            currentMatch.scores[0] += 1
+            if turnP1:
+                conn.send(f"NOTICE:Waiting for [{nomeServidor}]...".encode())
+                deadMansThread = threading.Thread(target=deadMansTrigger)
+                deadMansThread.start()  ## INICIA CONTAR O TEMPO PARA RESPONDER
+                col = NULL
+                while col == NULL:
+                    if fila.get() > 60:
+                        result = 2
+                        col = "TRIGGERACTIVATED"
+                    col = g.playerInput(1, currentMatch)
+                deadMansThread.join() ## FINALIZA A CONTAGEM DO TEMPO PARA RESPONDER
+                time.sleep(0.5)
+                if col == "TRIGGERACTIVATED":
+                    GAME = False
+                else:
+                    conn.send(f"REMOTEPLAY {col}".encode())
+                    row = g.addPiece(1, col, currentMatch)
+                    result = g.checkWin(1, row, col, currentMatch)
+                    turnP1 = False
+            else:
+                print(f"Waiting for [{nomeCliente}]...")
+                conn.send("ASKPLAY".encode())
+                time.sleep(0.5)
+                col = NULL
+                while col == NULL:
+                    if fila.get() > 60:
+                        result = 1
+                        col = "TRIGGERACTIVATED"
+                    col = int(conn.recv(1024).decode())
+                row = g.addPiece(2, col, currentMatch) # vai ter que ser repetido em cliente
+                result = g.checkWin(2, row, col, currentMatch)
+                turnP1 = True
+
+            time.sleep(0.5)
+            g.showBoard(currentMatch)
+            conn.send("SHOWBOARD".encode())
+            time.sleep(0.5)
+
+            if result == 1 or result == 2: # finaliza
+                if(result == 1):
+                    msg = f'Player [{nomeServidor}] wins!'
+                    currentMatch.scores[PLAYER1] += 1
+                elif(result == 2):
+                    msg = f'Player [{nomeCliente}] wins!'
+                    currentMatch.scores[PLAYER2] += 1
+                GAME = False
+                print(msg)
+                conn.send(f'NOTICE:{msg}'.encode())
+            elif result == -1:
+                msg = 'Draw!'
+                currentMatch.scores[PLAYER2] += 1
+                GAME = False
+                print(msg)
+                conn.send(f'NOTICE:{msg}'.encode())
+        valid = True
+        conn.send('REPLAY'.encode())
+        replayClient = conn.recv(1024).decode()
+        while valid:
+            replayServer = input("Replay?(y/n): ").lower()
+            if replayServer == "n" or replayClient == "n":
+                if replayServer == 'n' and replayClient == 'y':
+                    msg = f'[{nomeServidor}] quit'
+                elif replayServer == 'y' and replayClient == 'n':
+                    msg = f'[{nomeCliente}] quit'
+                elif replayServer == 'n' and replayClient == 'n':
+                    msg = f'Quitting game'
+                conn.send(f'NOTICE:{msg}'.encode())
+                print(f'{msg}')
+                time.sleep(0.5)
+                conn.send('QUIT'.encode())
+                valid = False
+                QUIT = True
+            elif replayServer == "y" and replayClient == "y":
+                valid = False
+                GAME = True
+
+    conn.close() # close the connection
+
+if __name__ == '__main__':
+    fila = queue.Queue()
+    serverThread = threading.Thread(target=serverProgram(), args=(fila,))
